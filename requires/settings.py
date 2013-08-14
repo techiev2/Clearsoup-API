5a5f42dc99cc6248410ccba72a86ec8d7ccded0c
somesh <singh0213somesh@gmail.com>
2013-08-14 18:06:29 +0530

    added orgnization in app setttings

--- requires/settings.py
+++ requires/settings.py
@@ -29,7 +29,7 @@ class ClearSoupApp(Application, object):
 GEN_PATH = lambda path: os.path.join(os.getcwd(), path)
 
 SETTINGS = {
-    'APPS': [ 'project', 'story', 'auth', 'core', 'user'],
+    'APPS': [ 'project', 'story', 'auth', 'core', 'user', 'organization'],
 	# Security
     'cookie': 'token',  # Specify the cookie variable name
     'login_url': '/api/authenticate/',  # Login path for the application
