b1f2423cde1eec71bd838e01f10fc41735cfee62
somesh <singh0213somesh@gmail.com>
2013-08-14 18:05:29 +0530

    added url map for org settings

--- organization/urls.py
+++ organization/urls.py
@@ -4,9 +4,11 @@ Created on 13-Aug-2013
 @author: someshs
 '''
 
-from organization.handler import OrganizationHandler
+from organization.handler import OrganizationHandler, OrgProfileHandler
 
-URLS = [('/api/projects/?$', OrganizationHandler),
+URLS = [('/api/organization/?$', OrganizationHandler),
+        ('/api/organization/(?P<organization>.*?)/settings/?$', OrgProfileHandler),
+        ('/api/organization/(?P<organization>.*?)/?$', OrganizationHandler),
        ]
 
 __all__ = ['URLS']
