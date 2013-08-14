92f3930a7a88cbd546f58a6c9dbeb39616f68bc3
somesh <singh0213somesh@gmail.com>
2013-08-14 12:46:17 +0530

    project datamodel modification. added get current sprint and modified the sprint create method

--- datamodels/project.py
+++ datamodels/project.py
@@ -16,11 +16,8 @@ from utils.dumpers import json_dumper
 
 
 class Project(me.Document):
-    '''
-        Basic project fields.
-    '''
-    
-#    organization = me.ReferenceField('Oraganization')
+    organization = me.ReferenceField('Organization',required=False,
+                                     reverse_delete_rule=me.CASCADE)
     # these four will come in request data for put
     title = me.StringField(max_length=64, required=True, unique=True)
     start_date = me.DateTimeField(default=datetime.utcnow())
@@ -31,8 +28,9 @@ class Project(me.Document):
     sequence = me.IntField()
     sprints = me.ListField()
     members = me.ListField()
-    admin = me.ListField()
-    active = me.BooleanField(default=True)
+    admin = me.ListField(me.ReferenceField('User'))
+    is_active = me.BooleanField(default=True)
+    description = me.StringField(max_length=500)
     
     # these have to be moved to base model
     created_by = me.ReferenceField('User', required=True)
@@ -40,32 +38,36 @@ class Project(me.Document):
     created_at = me.DateTimeField(default=datetime.utcnow())
     updated_at = me.DateTimeField(default=datetime.utcnow())
     deleted_at = me.DateTimeField()
+    deleted_by = me.ReferenceField('User')
     
 
-    @classmethod
-    def post_init(cls, sender, document, **kwargs):
-        pass
+    meta = {
+        'indexes': ['title']
+        }
+
+    def __init__(self, *args, **values):
+        super(Project, self).__init__( *args, **values)
 
     def __str__(self):
         return self.title
-    
+
     def to_json(self, fields=None, exclude=None):
         return json_dumper(self, fields, exclude)
-    
+
     def clean(self):
         Project.objects.filter(title=self.title).count()
         if Project.objects.filter(title=self.title).count() > 0:
             raise ValidationError('Duplicate project')
         if self.start_date > self.end_date:
             raise ValidationError('start date should be greater than end date.')
-    
+
     @classmethod
     def last_project_id(cls):
         sequence = None
         if Project.objects:
             sequence = list(Project.objects.order_by('sequence'))[-1].sequence
         return sequence
-    
+
     @classmethod
     def pre_save(cls, sender, document, **kwargs):
         '''
@@ -77,7 +79,9 @@ class Project(me.Document):
             document.sequence = last_sequence + 1
         else:
             document.sequence = 1
-    
+        if len(document.title) > 64:
+            ValidationError('Title exceeds 64 characters')
+
     @classmethod
     def post_save(cls, sender, document, **kwargs):
         '''
@@ -97,29 +101,35 @@ class Project(me.Document):
                     days=((idx - 1) * sprint_duration))
                 sprint_end_date = sprint_start_date + timedelta(
                     days=sprint_duration)
+                try:
+                    Sprint.objects.get(sequence=idx, project=self)
+                except Sprint.DoesNotExist:
+                    Sprint.objects.create(sequence=idx,
+                              project=self,
+                              start_date=sprint_start_date,
+                              end_date=sprint_end_date,
+                              created_by=self.created_by,
+                              updated_by=self.updated_by)
+            else: # for backlog
+                try:
+                    Sprint.objects.get(sequence=idx, project=self)
+                except Sprint.DoesNotExist:
+                    Sprint.objects.create(project=self,
+                              created_by=self.created_by,
+                              updated_by=self.updated_by)
 
-                (proj_sprint, created) = Sprint.objects.get_or_create(
-                                            sequence=idx,
-                                            project=self,
-                                            start_date=sprint_start_date,
-                                            end_date=sprint_end_date)
-            else:
-                # for backlog
-                (proj_sprint, created) = Sprint.objects.get_or_create(
-                                            sequence=idx,
-                                            project=self)
     def calculate_sprints(self):
         if not self.duration:
             self.duration = 7
         project_duration = (self.end_date - self.start_date).days
         sprints = project_duration / self.duration
-
+        
         # in case duration is of form 7K + 1, one sprint has to be added
         difference = project_duration - (self.duration * sprints)
 
         if difference > 0 and difference < 7:
             sprints += 1
-
+        
         sprint_list = ['Backlog']
         sprint_list.extend(['Sprint ' + str(
                             idx) for idx in range(1, sprints + 1)])
@@ -151,10 +161,24 @@ class Project(me.Document):
         super(Project, self).save(*args, **kwargs)
         self.calculate_sprints()
         self.reload()
-    
+
     def update(self, *args, **kwargs):
         super(Project, self).update(*args, **kwargs)
         self.reload()
+
+    def get_sprint_object(self, sequence):
+        try:
+            sprint = Sprint.objects.get(sequence=sequence,
+                               project=self)
+        except Sprint.DoesNotExist, error:
+            raise ValidationError(error)
+        return sprint
+
+    def get_story_list(self):
+        # import is done here in order to resolve cyclic import
+        from datamodels.story import Story
+        return Story.objects.filter(project=self, is_active=True
+                                    ).order_by('created_at')
 
 
 class Sprint(me.Document):
@@ -162,10 +186,26 @@ class Sprint(me.Document):
     start_date = me.DateTimeField()
     end_date = me.DateTimeField()
     sequence = me.IntField(required=True, default=0, unique_with='project')
-    project = me.ReferenceField('Project')
+    project = me.ReferenceField('Project', required=True,
+                                reverse_delete_rule=me.CASCADE)
+
+    created_at = me.DateTimeField(default=datetime.utcnow)
+    updated_at = me.DateTimeField(default=datetime.utcnow)
+    created_by = me.ReferenceField('User', required=False)
+    updated_by = me.ReferenceField('User', required=False)
+    is_active = me.BooleanField(default=True)
+
+    meta = {
+        'indexes': ['project']
+        }
+
+
+    def __str__(self):
+        return self.project.title +'-' + str(self.sequence)
 
     def save(self, *args, **kwargs):
         super(Sprint, self).save(*args, **kwargs)
+        self.reload()
 
     def update(self, *args, **kwargs):
         super(Sprint, self).update(*args, **kwargs)
@@ -178,4 +218,3 @@ class Sprint(me.Document):
 signals.pre_save.connect(Project.pre_save, sender=Project)
 signals.post_save.connect(Project.post_save, sender=Project)
 
-signals.post_init.connect(Project.post_init, sender=Project)
