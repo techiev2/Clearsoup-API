f72fe3f55c785d62a9372ed48073e1e2fda6ad41
somesh <singh0213somesh@gmail.com>
2013-08-20 12:08:42 +0530

    added url map for  the requests

--- organization/urls.py
+++ organization/urls.py
@@ -5,10 +5,22 @@ Created on 13-Aug-2013
 '''
 
 from organization.handler import OrganizationHandler, OrgProfileHandler
+from organization.project import OrgProjectHandler
+from organization.permission import OrgPermissionHandler,\
+ ProjectPermissionHandler
+from organization.team import TeamHandler
+from organization.members import OrgMemeberHandler
+
 
 URLS = [('/api/organization/?$', OrganizationHandler),
-        ('/api/organization/(?P<organization>.*?)/settings/?$', OrgProfileHandler),
-        ('/api/organization/(?P<organization>.*?)/?$', OrganizationHandler),
+        ('/api/(?P<organization>.*?)/project/?$', OrgProjectHandler),
+        ('/api/(?P<organization>.*?)/project/permission/?$',
+                                                ProjectPermissionHandler),
+        ('/api/(?P<organization>.*?)/permission/?$', OrgPermissionHandler),
+        ('/api/(?P<organization>.*?)/team/?$', TeamHandler),
+        ('/api/(?P<organization>.*?)/member/?$', OrgMemeberHandler),
+        ('/api/(?P<organization>.*?)/settings/?$', OrgProfileHandler),
+        ('/api/(?P<organization>.*?)/?$', OrganizationHandler),
        ]
 
 __all__ = ['URLS']
