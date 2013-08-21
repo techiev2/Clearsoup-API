d66f695098abc5aa0cf9592ff606c7d486ed1472
somesh <singh0213somesh@gmail.com>
2013-08-21 16:18:43 +0530

    added sprint module for organization

--- organization/project.py
+++ organization/project.py
@@ -91,3 +91,73 @@ class OrgProjectHandler(BaseHandler):
         if not self._headers_written:
             self.write(project.to_json())
 
+
+class SprintHandler(BaseHandler):
+    
+    SUPPORTED_METHODS = ('GET', 'PUT')
+    REQUIRED_FIELDS   = {
+        'PUT': ('projectId','sprints'),
+        'GET': ('projectId',)
+        }
+    data = {}
+    
+    def get_valid_project(self, project_id, organization):
+        if not project_id:
+            self.send_error(404)
+        try:
+            project = Project.get_project_object(sequence=project_id,
+                                                 organization=organization)
+            if self.current_user != project.admin:
+                self.send_error(404)
+        except ValidationError, error:
+            raise HTTPError(404, **{'reason': self.error_message(error)})
+        return project
+
+    def validate_request(self, organization):
+        if not organization:
+            self.send_error(400)
+        else:
+            try:
+                org = Organization.get_organization_object(organization)
+            except ValidationError, error:
+                raise HTTPError(404, **{'reason': self.error_message(error)})
+            return org
+
+    @authenticated
+    def get(self,*args, **kwargs):
+        organization = kwargs.get('organization', None)
+        org = self.validate_request(organization)
+        project_id = self.get_argument('projectId', None)
+        sprint_sequence = self.get_argument('sprint',None)
+        response = {}
+        project = self.get_valid_project(project_id, org)
+        if not sprint_sequence:
+            sprints = list(Sprint.objects.filter(project=project))
+            response['sprints'] = json_dumper(sprints)
+        elif sprint_sequence:
+            try:
+                sprint = project.get_sprint_object(sprint_sequence)
+                response['sprint'] = sprint.to_json()
+                response['stories'] = json_dumper(list(sprint.get_stories()))
+            except ValidationError, error:
+                raise HTTPError(404, **{'reason': self.error_message(error)})
+        response['project'] = project.to_json()
+        self.finish(json.dumps(response))
+
+    @authenticated
+    def put(self, *args, **kwargs):
+        organization = kwargs.get('organization', None)
+        org = self.validate_request(organization)
+        project_id = self.data.get('projectId', None)
+        number_of_sprints = self.data.get('sprints', None)
+        project = self.get_valid_project(project_id, org)
+        response = {}
+        for each in xrange(number_of_sprints):
+            try:
+                sprint = project.add_sprint(self.current_user)
+                response['Sprint :' + str(sprint.sequence)] = sprint.to_json()
+            except ValidationError, error:
+                raise HTTPError(500, **{'reason':self.error_message(error)})
+        self.write(response)
+
+
