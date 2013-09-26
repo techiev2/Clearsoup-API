e7a66b9f0e46f7c3f7f8dc79170c7d0c6f982a34
somesh <singh0213somesh@gmail.com>
2013-09-26 11:25:41 +0530

    modified task handler for task deletion

--- task/handler.py
+++ task/handler.py
@@ -11,8 +11,10 @@ from requires.base import BaseHandler, authenticated, validate_path_arg
 from datamodels.project import Project
 from datamodels.story import Story
 from datamodels.task import Task
+from datamodels.permission import ProjectPermission
 from utils.app import millisecondToDatetime
 from utils.dumpers import json_dumper
+from requires.settings import PROJECT_PERMISSIONS
 
 
 class TaskHandler(BaseHandler):
@@ -22,7 +24,7 @@ class TaskHandler(BaseHandler):
         'POST': ('projectId','taskId'),
         'PUT': ('projectId', 'title','storyId', 'estimated_completion_date', 
                 'parentTaskId'),
-        'DELETE' : ('projectId','taskId'),
+        'DELETE' : ('tasks',),
         }
     data = {}
     
@@ -47,11 +49,25 @@ class TaskHandler(BaseHandler):
             self.data['created_by'] = self.current_user
         self.data['updated_by'] = self.current_user
 
-    def get_project_object(self, sequence):
-        try:
-            project = Project.get_project_object(sequence=sequence)
-        except ValidationError, error:
-            raise HTTPError(404, **{'reason': self.error_message(error)})
+    def get_project_object(self, project_id=None, permalink=None):
+        if not project_id and not permalink:
+            self.send_error(404)
+        if project_id:
+            try:
+                project = Project.get_project_object(sequence=project_id)
+                if self.current_user not in project.members:
+                    self.send_error(404)
+            except ValidationError, error:
+                raise HTTPError(404, **{'reason': self.error_message(error)})
+        elif permalink:
+            try:
+                project = Project.objects.get(
+                            permalink__iexact=permalink,
+                        )
+                if not self.current_user in project.members:
+                    raise HTTPError(403)
+            except ValidationError, error:
+                raise HTTPError(404, **{'reason': self.error_message(error)})
         return project
 
     def get_story_object(self, project=None, sequence=None):
@@ -70,6 +86,30 @@ class TaskHandler(BaseHandler):
             except Task.DoesNotExist:
                 pass
         return task
+
+    def check_permission(self, permission):
+        permission_flag = False
+        if ProjectPermission.testBit(permission.map,
+                             PROJECT_PERMISSIONS.index('can_delete_task')):
+            permission_flag = True
+        return permission_flag
+
+    def validate_tasks(self, project=None, tasks=None):
+        '''
+        This method validates the tasks which are send from web to delete
+        '''
+        flag = False
+        if tasks:
+            id = 0
+            for id, task in enumerate(tasks):
+                try:
+                    Task.objects.get(sequence=int(task),
+                                      project=project)
+                except Task.DoesNotExist:
+                    msg = 'Task story sequences'
+                    raise HTTPError(500, **{'reason':msg})
+            if id == len(tasks) - 1: flag = True
+        return flag 
 
     @authenticated
     def get(self,*args, **kwargs):
@@ -120,15 +160,38 @@ class TaskHandler(BaseHandler):
     @authenticated
     def delete(self, *args, **kwargs):
         project_id = self.get_argument('projectId', None)
-        task_id = self.get_argument('taskId',None)
+        project_permalink = self.get_argument('project_permalink', None)
+        tasks = self.get_arguments('tasks', None)
         response = {}
+        project = None
         if project_id:
-            project = self.get_project_object(project_id)
-            if task_id:
-                task = self.get_task_object(project, task_id)
-                if not task:
-                    raise HTTPError(404, **{'reason': 'Task not found'})
-                else:
-                    task.update(set__is_active=False)
-                    response['task'] = task.to_json()
-        self.finish(response)
+            project = self.get_project_object(project_id=project_id,
+                                              permalink=None)
+        elif project_permalink:
+            project = self.get_project_object(project_id=None,
+                                              permalink=project_permalink)
+        if tasks:
+            permission = None
+            try:
+                permission = ProjectPermission.objects.get(project=project,
+                                                       user=self.current_user)
+            except ProjectPermission.DoesNotExist:
+                msg = 'Not authorized to delete tasks of this project'
+                raise HTTPError(500, **{'reason':msg})
+            if self.check_permission(permission):
+                if self.validate_tasks(project=project, tasks=tasks):
+                    for task in tasks:
+                        try:
+                            task = Task.objects.get(sequence=int(task),
+                                                      project=project,
+                                                      is_active=True)
+                            task.update(set__is_active=False)
+                        except Task.DoesNotExist, error:
+                            raise HTTPError(500, **{'reason':self.error_message(error)})
+                    response = {'message': 'Successfully deleted.',
+                                'status': 200}
+            else:
+                msg = 'Not authorized to delete task of this project'
+                raise HTTPError(500, **{'reason':msg})
+        self.finish(json.dumps(response))
+
