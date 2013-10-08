33ecd29c7c7865d94423d7c8c87dfdac612191cd
somesh <singh0213somesh@gmail.com>
2013-10-08 15:44:13 +0530

    added url map for task comment

--- task/urls.py
+++ task/urls.py
@@ -3,9 +3,10 @@ Created on 23-Aug-2013
 
 @author: someshs
 '''
-from task.handler import TaskHandler
+from task.handler import TaskHandler, TaskCommentHandler
 
 URLS = [('/api/task/?$', TaskHandler),
+        ('/api/task-comment/?$', TaskCommentHandler)
        ]
 
 __all__ = ['URLS']
