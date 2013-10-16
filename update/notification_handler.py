8765b131003671b54b6c64301aae3c19fbe6629b
Srinath Sankar <srinath@iambot.net>
2013-10-16 17:16:48 +0530

    Move notification websocket to web handler

--- update/notification_handler.py
+++ update/notification_handler.py
@@ -13,35 +13,24 @@ from utils.dumpers import json_dumper
 
 class NotificationHandler(BaseHandler):
 
-    SUPPORTED_METHODS = ('GET','POST',)
+    SUPPORTED_METHODS = ('GET',)
 
     @authenticated
     def get(self, *args, **kwargs):
-        notifications = Notification.objects(
-            for_user=self.current_user,
-            is_read=False
-            ).order_by('-id')[self._limit_from:self._limit_to]
+        qParams = {
+            'for_user': self.current_user,
+            'is_read': False
+        }
+        last_id = self.get_argument('last_id', None)
+        if last_id:
+            qParams['id__gt'] = last_id
+        
+        notifications = Notification.objects(**qParams)\
+            .order_by('-id')[self._limit_from:self._limit_to]
 
         response = json_dumper(notifications)
         self.finish(json.dumps(response))
 
-    @authenticated
-    def post(self, *args, **kwargs):
-        print NotificationManager.createNotification(for_user=self.current_user)
-
-
-class NotificationWebSocket(WebSocketHandler):
-    def open(self):
-        print "WebSocket opened"
-        # Once the websocket connection is open, we need
-        # to continuously push stuff
-        IOLoop.current().add_timeout(datetime.timedelta(seconds=5), self.test)
-
-    def on_message(self, message):
-        self.write_message(u"You said: " + message)
-
-    def on_close(self):
-        print "WebSocket closed"
-
-    def test(self):
-    	print 'testing'
+    # @authenticated
+    # def post(self, *args, **kwargs):
+    #     print NotificationManager.createNotification(for_user=self.current_user)
