49aac48495a55193192bba273c3cf2f81a76b0d1
Sriram Velamur <sriram.m@imaginea.com>
2013-11-25 15:16:45 +0530

    Adds password reset handler

--- profile/urls.py
+++ profile/urls.py
@@ -4,10 +4,13 @@ Created on 06-Aug-2013
 @author: someshs
 '''
 
-from profile.handler import ProfileHandler
+from profile.handler import ProfileHandler, ResetPasswordHandler
 
-URLS = [('/api/profile/?$', ProfileHandler),
-       ]
+URLS = [
+    ('/api/profile/?$', ProfileHandler),
+    (r'^/api/reset-password-init/?$', ResetPasswordHandler),
+    (r'^/api/reset-password/(?P<token>.*?)/?$', ResetPasswordHandler)
+]
 
 __all__ = ['URLS']
 
